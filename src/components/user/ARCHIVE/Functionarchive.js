1aacf7e01beb2a5fbaf36953d722a61edb957711
yazidam <ahmedyazid.mejri@esprit.tn>
2021-04-30 15:55:21 +0100

    Fix Archive

---
+++ src/components/user/ARCHIVE/Functionarchive.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useEffect } from 'react';
+
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  loginUserfind,
+  selectConnectuser,
+} from '../../.././redux/slices/userSlice';
+import Archiveciruitdeliveruman from './Archiveciruitdeliveruman';
+
+const Functionarchive = (props) => {
+  const [connectUser, error] = useSelector(selectConnectuser);
+  const dispatch = useDispatch();
+
+  useEffect(async () => {
+    if (Cookies.get('connect.sid')) {
+    } else {
+      await axios
+        .get('http://localhost:5000/auth/logout', { withCredentials: true })
+        .then((res) => {
+          console.log(res);
+          localStorage.removeItem('userInfo');
+          dispatch(loginUserfind(res.data));
+          props.history.push('/');
+        });
+    }
+  }, [Cookies.get()]);
+  return (
+    <div style={{ height: '700px' }}>
+      <h1>Archive</h1>
+      <Archiveciruitdeliveruman con={connectUser} />
+    </div>
+  );
+};
+
+export default Functionarchive;
