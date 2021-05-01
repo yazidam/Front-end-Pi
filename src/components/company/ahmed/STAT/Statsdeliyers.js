a8d7d79e8d3c2da391ea4384dde430bcbe592e90
yazidam <ahmedyazid.mejri@esprit.tn>
2021-05-01 14:07:34 +0100

    Fixe Err

--- src/components/company/ahmed/STAT/Statsdeliyers.js
+++ src/components/company/ahmed/STAT/Statsdeliyers.js
@@ -1,128 +1,37 @@
-// import React, { Component } from 'react';
-// //import { Bar, Line, Pie } from 'react-chartjs-2';
-// import axios from 'axios';
-// import {
-//   PieChart,
-//   Pie,
-//   Tooltip,
-//   BarChart,
-//   XAxis,
-//   YAxis,
-//   Legend,
-//   CartesianGrid,
-//   Bar,
-// } from 'recharts';
-// export default class Statsdeliyers extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       tabstat: [],
-//       a: 0,
-//       b: 0,
-//     };
-//   }
-
-//   componentDidMount() {
-//     this.getVehicules();
-//   }
-
-//   getVehicules() {
-//     axios
-//       .get('http://localhost:5000/delivery/stat_all_deliverys', {
-//  withCredentials: true,
-//       })
-//       .then((res) => {
-//         console.log(res.data);
-//         this.setState({
-//           tabstat: res.data.data,
-//         });
-//         console.log('tabstat: ', this.state.tabstat);
-//       });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>stattttttttttttttttttttttttttttttttttttttt</h1>
-//         <div style={{ textAlign: 'center' }}>
-//           <h1>Socail Media Users</h1>
-//           <div className="App">
-//             <PieChart width={400} height={400}>
-//               <Pie
-//                 dataKey="count"
-//                 isAnimationActive={false}
-//                 data={this.state.tabstat}
-//                 cx={200}
-//                 cy={200}
-//                 outerRadius={80}
-//                 fill="#8884d8"
-//                 label
-//               />
-//               <Tooltip />
-//             </PieChart>
-//             <BarChart
-//               width={500}
-//               height={300}
-//               data={this.state.tabstat}
-//               margin={{
-//                 top: 5,
-//                 right: 30,
-//                 left: 80,
-//                 bottom: 5,
-//               }}
-//               barSize={20}
-//             >
-//               <XAxis
-//                 dataKey="_id"
-//                 scale="point"
-//                 padding={{ left: 10, right: 10 }}
-//               />
-//               <YAxis />
-//               <Tooltip />
-//               <Legend />
-//               <CartesianGrid strokeDasharray="3 3" />
-//               <Bar
-//                 dataKey="count"
-//                 fill="#8884d8"
-//                 background={{ fill: '#eee' }}
-//               />
-//             </BarChart>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
 import axios from 'axios';
 import React, { Component } from 'react';
 import { Bar, Line, Pie } from 'react-chartjs-2';
 import { useState, useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  loginUserfind,
+  selectConnectuser,
+} from '../../../../redux/slices/userSlice';
 
-function Statsdeliyers() {
+function Statsdeliyers(props) {
   const [chartData, SetchartData] = useState({});
-  //   const { id } = useParams();
-  //   axios.defaults.headers.common['Authorization'] =
-  //     'bearer ' + localStorage.getItem('Data');
-
-  //   const [chartData, SetchartData] = useState({});
-
-  //   var local = localStorage.getItem('Data');
-  //   if (local !== 'undefined' && local !== null) {
-  //     const decode = jwt_decode(local);
-  //     var Username = decode.username;
-  //     var iduser = decode._id;
-  //     console.log('idusssserr' + iduser);
-  //   } else {
-  //     //  history.push("/");
-  //     console.log('mat3adash');
-  //     //history.push('/')
-  //   }
+  const [connectUser, error] = useSelector(selectConnectuser);
+  const dispatch = useDispatch();
+  useEffect(async () => {
+    if (Cookies.get('connect.sid')) {
+    } else {
+      await axios
+        .get('http://localhost:5000/auth/logout', { withCredentials: true })
+        .then((res) => {
+          console.log(res);
+          localStorage.removeItem('userInfo');
+          dispatch(loginUserfind(res.data));
+          props.history.push('/');
+        });
+    }
+  }, [Cookies.get()]);
 
   const chart = () => {
     let cat = [];
     let nombre = [];
-    // console.log('RRRRRRRRRRRR' + iduser);
+
     axios
       .get('http://localhost:5000/delivery/stat_all_deliverys', {
         withCredentials: true,
