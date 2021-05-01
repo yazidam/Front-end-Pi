1f32a2d4191c05c2d80dbc447c2a0b3357401bdb
yazidam <ahmedyazid.mejri@esprit.tn>
2021-05-01 11:53:07 +0100

    Adding Profile User

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -24,7 +24,49 @@ export default function Profile(props) {
 
   return (
     <div style={{ height: '700px' }}>
-      <h1>i'm profile</h1>
+      <body className="bg-light">
+        <div className="container">
+          <h1>i'm profile</h1>
+          <div className="row d-flex justify-content-center">
+            <div className="col-md-10 mt-5 pt-5">
+              <div className="row z-depth-3">
+                <div className="col-sm-4 bg-info rounded-left">
+                  <div className="card-block text-center text-white">
+                    <i className="fas fa-user-tie fa-7x mt-5" />
+                    <h2 className="font-weight-bold mt-4">
+                      {connectUser.username}
+                    </h2>
+                    <p>Your Profile</p>
+                    <i className="far fa-edit fa-2x mb-4"></i>
+                  </div>
+                </div>
+                <div className="col-sm-8 bg-white rounded-right">
+                  <h3 className="mt-3 text-center"> Information</h3>
+                  <hr className="badge-primary mt-0 " />
+                  <div className="row">
+                    <div className="col-sm-6">
+                      <p className="font-weight-bold"> Email</p>
+                      <h6 className="text-muted">{connectUser.email}</h6>
+                    </div>
+                    <div className="col-sm-6">
+                      <p className="font-weight-bold"> Adresse</p>
+                      <h6 className="text-muted">{connectUser.adresse}</h6>
+                    </div>
+                    <div className="col-sm-6">
+                      <p className="font-weight-bold"> Phone</p>
+                      <h6 className="text-muted">{connectUser.phone}</h6>
+                    </div>
+                    <div className="col-sm-6">
+                      <p className="font-weight-bold"> Role</p>
+                      <h6 className="text-muted">{connectUser.role}</h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
